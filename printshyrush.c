90ffad7367feb66783b796a19e19d906eeb69c5b
ShyrushShrestha <Shyrush.sresta1@gmail.com>
2016-10-17 10:59:56 +0545

    Add files via upload

---
+++ printshyrush.c
@@ -0,0 +1,16 @@
+
+/*
+Student name : Shyrush Shrestha
+Subject:Programming Fundam.
+LabNo: 01
+Program:Print your name
+Date : 2016-10-17
+*/
+
+#include<stdio.h>	//printf,scanf() infohold
+//#include<conio.h>	//getch(),clrscr() infohold
+
+int main(){
+printf("my name is shyrush shrestha");	
+return (0);
+	}
